426f6082b0a3731235bc590e9c56ad83ce4ebb59
skiym-aaa <y.sakiyama3216@gmail.com>
2020-06-27 03:15:28 +0000

    Make a basic User model (including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name:"Example User", email: "Example User")
+    @user = User.new(name:"Example User", email: "Example User", password: "foobar", password_confirmation: "foobar")
   end
 
   # test "should be valid" do
@@ -34,5 +34,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present(nonblank)" do
+    @user.password = @user.password_confirmation = "" * 6
+    assert_not @user.valid?
+  end
+
+  test "password should be minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
