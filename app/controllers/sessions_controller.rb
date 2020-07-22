306e7fb4b093b456b3e2f2574c45b223a5c3b239
skiym-aaa <y.sakiyama3216@gmail.com>
2020-07-22 09:21:56 +0000

    Finish 8-2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] ='invalid email/password combination'
       render 'new'
